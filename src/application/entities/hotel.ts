4331fbfbf1fbbaeb051ee1b7b3827a2d511cab21
Liv Audigane <laudigane@lexpress.fr>
2025-03-28 14:15:03 +0100

    Domain Driven Design

--- src/application/entities/hotel.ts
+++ src/application/entities/hotel.ts
@@ -3,6 +3,7 @@ import { Room } from './Room';
 export class Hotel {
   constructor(public readonly rooms: Room[]) {}
 
+  // en charge de la cohérence de lui-même et de ses enfants => agrégat
   public setAllRoomsPrices(groundFloorPrice: number) {
     this.rooms.forEach((room) => {
       room.changePrice(groundFloorPrice);
--- src/application/use-cases/FixRoomPricesUseCase.ts
+++ src/application/use-cases/FixRoomPricesUseCase.ts
@@ -1,5 +1,5 @@
-import { Hotel } from '../entities/hotel';
 import { IRoomRepository } from '../presenters/IRoomRepository';
+import { Hotel } from '../entities/Hotel';
 
 export class RoomSetterUseCase {
   constructor(private readonly roomRepository: IRoomRepository) {}
